caa5a504d4dae357eab60e2f6679456713174f9c
mostafa <mostafa@how.fm>
2025-11-30 01:19:12 +0100

    feat(types): Add parsing types export to central index for improved type management and accessibility.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export * from './auth.types';
 export * from './cloudflare';
 export * from './file.types';
+export * from './parsing.types';
 export * from './requests';
 export * from './responses';
